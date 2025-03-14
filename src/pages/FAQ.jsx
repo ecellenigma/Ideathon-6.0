e45805cd36a9a943fa7da1b3364e9cfb07854e84
PolarPhoenix <rosyrdas@gmail.com>
2025-03-14 20:34:41 +0530

    feat: put actual FAQs

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -30,17 +30,13 @@ return (
                   className="h-4 w-4"
                 />
               </div>
-            <span className="font-semibold">What is crypto capital?</span>
+            <span className="font-semibold">What is Ideathon?</span>
             </div>
         </AccordionTrigger>
         <AccordionContent className="px-4 py-3 bg-[#192238]">
             <p className="text-sm text-gray-300">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley
-            of type and scrambled it to make a type specimen book. It has
-            survived not only five centuries, but also the leap into
-            electronic typesetting.
+            An Ideathon is a competitive pitching event where teams present innovative ideas or solutions to real-world problems before a panel of experts. 
+            Participants are evaluated based on criteria such as creativity, feasibility, social impact, and other key factors.
             </p>
         </AccordionContent>
         </AccordionItem>
@@ -58,13 +54,12 @@ return (
                   className="h-4 w-4"
                 />
             </div>
-            <span className="font-semibold">Where are the verified contacts?</span>
+            <span className="font-semibold">Is accommodation provided for participants?</span>
             </div>
         </AccordionTrigger>
         <AccordionContent className="px-4 py-3 bg-[#192238]">
             <p className="text-sm text-gray-300">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
+            Yes, accommodation is available for participants attending the final round.
             </p>
         </AccordionContent>
         </AccordionItem>
@@ -82,15 +77,17 @@ return (
                   className="h-4 w-4"
                 />
               </div>
-            <span className="font-semibold">How is daily revenue distributed?</span>
+            <span className="font-semibold">Can we change the track during the event?</span>
             </div>
         </AccordionTrigger>
         <AccordionContent className="px-4 py-3 bg-[#192238]">
             <p className="text-sm text-gray-300">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            No, once a team selects a track, it cannot be changed during the event.
             </p>
         </AccordionContent>
         </AccordionItem>
+
+
 
         <AccordionItem
         value="item-4"
@@ -106,13 +103,66 @@ return (
                 />
               </div>
             <span className="font-semibold">
-                Why does the website occasionally not display certain data?
+              What if prototype gets damaged while coming on the way?
             </span>
             </div>
         </AccordionTrigger>
         <AccordionContent className="px-4 py-3 bg-[#192238]">
             <p className="text-sm text-gray-300">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            If your prototype sustains any damage while in transit, please reach out to us immediately. We will assess the situation and provide an appropriate solution to address the issue.
+            </p>
+        </AccordionContent>
+        </AccordionItem>
+
+
+        <AccordionItem
+        value="item-5"
+        className="border border-[#1E2A3E] rounded-lg overflow-hidden"
+        >
+        <AccordionTrigger className="flex items-center justify-between px-4 py-3 bg-[#141E2F] hover:bg-[#1a2537]">
+            <div className="flex items-center space-x-3">
+            <div className="h-8 w-8 flex items-center justify-center bg-[#1E2A3E] rounded-full">
+                <img
+                  src="src/assets/question mark.svg"
+                  alt="Question Mark"
+                  className="h-4 w-4"
+                />
+              </div>
+            <span className="font-semibold">
+            What should I do if I encounter logistical issues with the prototype?
+            </span>
+            </div>
+        </AccordionTrigger>
+        <AccordionContent className="px-4 py-3 bg-[#192238]">
+            <p className="text-sm text-gray-300">
+            If you face any logistical challenges related to the prototype, please reach out to us via email or text for assistance.
+            </p>
+        </AccordionContent>
+        </AccordionItem>
+
+
+
+        <AccordionItem
+        value="item-6"
+        className="border border-[#1E2A3E] rounded-lg overflow-hidden"
+        >
+        <AccordionTrigger className="flex items-center justify-between px-4 py-3 bg-[#141E2F] hover:bg-[#1a2537]">
+            <div className="flex items-center space-x-3">
+            <div className="h-8 w-8 flex items-center justify-center bg-[#1E2A3E] rounded-full">
+                <img
+                  src="src/assets/question mark.svg"
+                  alt="Question Mark"
+                  className="h-4 w-4"
+                />
+              </div>
+            <span className="font-semibold">
+              Is there a registration fee?
+            </span>
+            </div>
+        </AccordionTrigger>
+        <AccordionContent className="px-4 py-3 bg-[#192238]">
+            <p className="text-sm text-gray-300">
+            Yes, teams advancing to the final round are required to pay a one-time registration fee of ₹150.
             </p>
         </AccordionContent>
         </AccordionItem>
