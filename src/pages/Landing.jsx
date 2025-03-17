1c6d45765317fa0a4720c8e2fd699137f9c5762d
Surya prakash <suryaharsh279@gmail.com>
2025-03-17 11:53:41 +0530

    brochure added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,9 +2,8 @@ import React from "react";
 /* eslint-disable-next-line */
 import { motion } from "framer-motion";
 import heroSvg from "../assets/icontop.svg";
-
 import Navbar from "../components/Navbar";
-
+import brochure from "../../public/brochure.pdf"
 import RedoAnimText from "../components/AnimatedText";
 
 
@@ -26,37 +25,46 @@ const Landing = () => {
                             className="h-60 w-full relative z-10"
                             src={heroSvg}
                             alt=""
-                            initial={{ scale: 1, filter: "drop-shadow(0px 0px 0px rgba(255,255,255,0))" }} 
+                            initial={{ scale: 1, filter: "drop-shadow(0px 0px 0px rgba(255,255,255,0))" }}
                             whileHover={{
-                                scale: 1.1, 
-                                filter: "drop-shadow(0px 0px 30px rgba(255,255,255,0.8))", 
+                                scale: 1.1,
+                                filter: "drop-shadow(0px 0px 30px rgba(255,255,255,0.8))",
                             }}
-                            transition={{ duration: 0.2, ease: "easeInOut" }} 
+                            transition={{ duration: 0.2, ease: "easeInOut" }}
                         />
                     </div>
                 </div>
-
-                {/* Animated Text */}
                 <div className="w-full h-[100px] flex items-center justify-center overflow-hidden">
                     <RedoAnimText />
                 </div>
 
                 <div>
-                    <h1  className="text-2xl font-tektur md:text-3xl font-bold"> Innovators  Ka
+                    <h1 className="text-2xl font-tektur md:text-3xl font-bold"> Innovators  Ka
                         Apna  Battlefield
                     </h1>
                 </div>
-                <motion.button
-                    whileHover={{ scale: 1.05 }}
-                    onClick={() => window.open("https://unstop.com/o/gsh9n8J?lb=4jXsa3G&utm_medium=Share&utm_source=shortUrl", "_blank")}
-                    whileTap={{ scale: 0.95 }}
-                    className="px-6 py-2 rounded cursor-pointer mt-10 font-medium bg-[#00C4F4] text-white transition-all cursor-alias  shadow-[3px_3px_0px_white] hover:shadow-none hover:translate-x-[3px]  hover:translate-y-[3px]"
-                >
-                    Apply Now
-                </motion.button>
+                <div className="flex gap-7 md:gap-8 items-center justify-center">
+                    <motion.button
+                        whileHover={{ scale: 1.05 }}
+                        onClick={() => window.open("https://unstop.com/o/gsh9n8J?lb=4jXsa3G&utm_medium=Share&utm_source=shortUrl", "_blank")}
+                        whileTap={{ scale: 0.95 }}
+                        className="px-6 py-2 rounded cursor-pointer mt-10 font-medium bg-[#00C4F4] text-white transition-all cursor-alias  shadow-[3px_3px_0px_white] hover:shadow-none hover:translate-x-[3px]  hover:translate-y-[3px]"
+                    >
+                        Apply Now
+                    </motion.button>
+                    <motion.button
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
+                        onClick={() => window.open(brochure, '_blank')} // Opens PDF in new tab
+                        className="px-6 py-2 rounded cursor-pointer mt-10 font-medium bg-[#060505] text-white transition-all cursor-alias shadow-[3px_3px_0px_white] hover:shadow-none hover:translate-x-[3px] hover:bg-black hover:translate-y-[3px]"
+                    >
+                        View Brochure
+                    </motion.button>
+                    
+                </div>
 
             </motion.div>
-           
+
 
         </section>
     );
